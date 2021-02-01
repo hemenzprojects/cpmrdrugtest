2f0308fdc56b25ab9216a86dc651420a60c1a153
Hemen Mike <hemengmike@gmail.com>
2021-02-01 09:58:28 +0000

    user config created and product report is sorted by analysedbyid 3

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -17,7 +17,6 @@ class DatabaseSeeder extends Seeder
         $this->call(ProductTypeTableSeeder::class);
         $this->call(ProductTableSeeder::class);
         $this->call(DeptTypeTableSeeder::class);
-        $this->call(DeptOfficeTableSeeder::class);
         $this->call(DeptTableSeeder::class);
         $this->call(ProductDeptSeeder::class);
         $this->call(MicrobialLoadSeeder::class);
@@ -30,6 +29,7 @@ class DatabaseSeeder extends Seeder
         $this->call(PhytoPhytochemicalSeeder::class);
         $this->call(PharmStandardSeeder::class);
         $this->call(AnimalModelSeeder::class);
+        $this->call(DeptOfficeTableSeeder::class);
 
 
 
