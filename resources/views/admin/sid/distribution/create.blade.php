a4ce5bc365b5a0c22ebbc600676b469c94932ef1
Hemen Mike <hemengmike@gmail.com>
2021-04-12 17:08:37 +0000

    sid distribution

--- resources/views/admin/sid/distribution/create.blade.php
+++ resources/views/admin/sid/distribution/create.blade.php
@@ -152,7 +152,7 @@
         <div class="modal-dialog" role="document">
           <div class="modal-content">
             <div class="modal-header">
-              <h5 class="modal-title" id="exampleModalLabel">New message</h5>
+              <h5 class="modal-title" id="exampleModalLabel"></h5>
               <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                 <span aria-hidden="true">&times;</span>
               </button>
@@ -166,40 +166,18 @@
                             <div class="form-group">
                                 <label for="exampleSelectGender">{{$dept[$n]->name}}</label><br>
                                 <select required name="product_id" style="width:100%;" class="form-control " tag="35909090456{{$n}}">
-                                   @if ($dept[$n]->id == 1)
-                                   @foreach (\App\Product::where('micro_grade',Null)->get() as $p)
-                                   @if (count($p->productDept()->where('dept_id',1)->get())>0)
-                                      @php continue; @endphp
-                                     
-                                   @endif
-                                
-                                 <option  value="{{$p->id}}" {{$p->id == old('product')? "selected":""}}>{{($p->code)}} - {{($p->name)}}</option>
-                            
-                                  @endforeach
-                                   @endif 
                                   
-                                   @if ($dept[$n]->id == 2)
-                                   @foreach (\App\Product::where('pharm_grade',Null)->get() as $p)
-                                   @if (count($p->productDept()->where('dept_id',2)->get())>0)
+                                   @foreach (\App\Product::all() as $p)
+                                   @if (count($p->productDept()->where('dept_id',$n)->get())>0)
                                       @php continue; @endphp
                                      
                                    @endif
                                 
-                                 <option  value="{{$p->id}}" {{$p->id == old('product')? "selected":""}}>{{($p->code)}} - {{($p->name)}}</option>
+                                 <option value="{{$p->id}}" {{$p->id == old('product')? "selected":""}}>{{($p->code)}} - {{($p->name)}} </option>
                             
                                   @endforeach
-                                   @endif 
-
-                                  @if ($dept[$n]->id == 3)
-                                   @foreach (\App\Product::where('phyto_grade',null)->get() as $p)
-                                   @if (count($p->productDept()->where('dept_id',3)->get())>0)
-                                      @php continue; @endphp
-                                   @endif
-
-                                  <option  value="{{$p->id}}" {{$p->id == old('product')? "selected":""}}>{{($p->code)}} - {{($p->name)}}</option>
-                            
-                                  @endforeach
-                                   @endif 
+                                  
+                             
                                 </select>
                             </div>
                         </div>
