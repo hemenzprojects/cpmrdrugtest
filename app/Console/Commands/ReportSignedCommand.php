55c84c903142a403f37a609c1944afe9564ca67e
Hemen Mike <hemengmike@gmail.com>
2021-05-04 08:48:31 +0000

    adding of sms to the sysytem

--- app/Console/Commands/ReportSignedCommand.php
+++ app/Console/Commands/ReportSignedCommand.php
@@ -39,60 +39,60 @@ class ReportSignedCommand extends Command
      *
      * @return mixed
      */
-    // public function handle()
-    // {
-    //        /** Phyto HoD SMS  **/
+    public function handle()
+    {
+           /** Phyto HoD SMS  **/
 
-    //      $phyto_p =  Product::where('phyto_hod_evaluation',0)->with('departments')->whereHas("departments", function($q){
-    //         return $q->where("dept_id", 3)->where("status", 3);
-    //        })->count();
-    //      $admin = Admin::findOrFail(4);
-    //      if ($phyto_p != 0) {
-    //          SendSMS::sendMessage('Hi '.$admin->full_name.',You have '.$phyto_p.' pending reports to sign. Kindly login to the drug analysis system and complete the process.',$admin->tell);
-    //      }
+         $phyto_p =  Product::where('phyto_hod_evaluation',0)->with('departments')->whereHas("departments", function($q){
+            return $q->where("dept_id", 3)->where("status", 3);
+           })->count();
+         $admin = Admin::findOrFail(4);
+         if ($phyto_p != 0) {
+             SendSMS::sendMessage('Hi '.$admin->full_name.',You have '.$phyto_p.' pending reports to sign. Kindly login to the drug analysis system and complete the process.',$admin->tell);
+         }
 
-    //         /** Micro HoD SMS  **/
+            /** Micro HoD SMS  **/
 
-    //       $micro_approval_1 = Product::where('micro_hod_evaluation',0)->where('micro_process_status','<',1)->with('departments')->whereHas("departments", function($q){
-    //         return $q->where("dept_id", 1)->where("status", 3);
-    //       })->count();
-    //       $micro_approval1_admin = Admin::findOrFail(4);
-    //       if ($micro_approval_1 != 0) {
-    //         SendSMS::sendMessage('Hi '.$micro_approval1_admin->full_name.',You have '.$micro_approval_1.' pending reports to sign. Kindly login to the drug analysis system and complete the process.',$micro_approval1_admin->tell);
-    //       }
+          $micro_approval_1 = Product::where('micro_hod_evaluation',0)->where('micro_process_status','<',1)->with('departments')->whereHas("departments", function($q){
+            return $q->where("dept_id", 1)->where("status", 3);
+          })->count();
+          $micro_approval1_admin = Admin::findOrFail(4);
+          if ($micro_approval_1 != 0) {
+            SendSMS::sendMessage('Hi '.$micro_approval1_admin->full_name.',You have '.$micro_approval_1.' pending reports to sign. Kindly login to the drug analysis system and complete the process.',$micro_approval1_admin->tell);
+          }
 
           
-    //       $micro_approval_2 = Product::where('micro_hod_evaluation',2)->where('micro_process_status',1)->with('departments')->whereHas("departments", function($q){
-    //         return $q->where("dept_id", 1)->where("status", 3);
-    //       })->count();
-    //       $micro_approval2_admin = Admin::findOrFail(4);
-    //       if ($micro_approval_2 != 0) {
-    //         SendSMS::sendMessage('Hi '.$micro_approval2_admin->full_name.',You have '.$micro_approval_2.' pending reports to sign. Kindly login to the drug analysis system and complete the process.',$micro_approval2_admin->tell);
-    //       }
+          $micro_approval_2 = Product::where('micro_hod_evaluation',2)->where('micro_process_status',1)->with('departments')->whereHas("departments", function($q){
+            return $q->where("dept_id", 1)->where("status", 3);
+          })->count();
+          $micro_approval2_admin = Admin::findOrFail(4);
+          if ($micro_approval_2 != 0) {
+            SendSMS::sendMessage('Hi '.$micro_approval2_admin->full_name.',You have '.$micro_approval_2.' pending reports to sign. Kindly login to the drug analysis system and complete the process.',$micro_approval2_admin->tell);
+          }
 
-    //         /** Pharm HoD SMS  **/
+            /** Pharm HoD SMS  **/
 
-    //       $pharm_approval_1 =  Product::where('pharm_hod_evaluation',0)->where('pharm_process_status',5)->with('departments')->whereHas("departments", function($q){
-    //         return $q->where("dept_id", 2)->where("status", 7);
-    //       })->with('animalExperiment')->whereHas("animalExperiment")->count();
+          $pharm_approval_1 =  Product::where('pharm_hod_evaluation',0)->where('pharm_process_status',5)->with('departments')->whereHas("departments", function($q){
+            return $q->where("dept_id", 2)->where("status", 7);
+          })->with('animalExperiment')->whereHas("animalExperiment")->count();
 
-    //      $pharm_approval1_admin = Admin::findOrFail(4);
-    //      if ($pharm_approval_1 != 0) {
-    //          SendSMS::sendMessage('Hi '.$pharm_approval1_admin->full_name.',You have '.$pharm_approval_1.' pending reports to sign. Kindly login to the drug analysis system and complete the process.',$pharm_approval1_admin->tell);
-    //      }
+         $pharm_approval1_admin = Admin::findOrFail(4);
+         if ($pharm_approval_1 != 0) {
+             SendSMS::sendMessage('Hi '.$pharm_approval1_admin->full_name.',You have '.$pharm_approval_1.' pending reports to sign. Kindly login to the drug analysis system and complete the process.',$pharm_approval1_admin->tell);
+         }
 
-    //      $pharm_approval_2 =  Product::where('pharm_hod_evaluation',2)->where('pharm_process_status',6)->with('departments')->whereHas("departments", function($q){
-    //         return $q->where("dept_id", 2)->where("status", 7);
-    //       })->with('animalExperiment')->whereHas("animalExperiment")->count();
+         $pharm_approval_2 =  Product::where('pharm_hod_evaluation',2)->where('pharm_process_status',6)->with('departments')->whereHas("departments", function($q){
+            return $q->where("dept_id", 2)->where("status", 7);
+          })->with('animalExperiment')->whereHas("animalExperiment")->count();
 
-    //      $pharm_approval2_admin = Admin::findOrFail(4);
-    //      if ($pharm_approval_2 != 0) {
-    //          SendSMS::sendMessage('Hi '.$pharm_approval2_admin->full_name.',You have '.$pharm_approval_2.' pending reports to sign. Kindly login to the drug analysis system and complete the process.',$pharm_approval2_admin->tell);
-    //      }
-
-
-    //     $this->info('SMS Sent');
+         $pharm_approval2_admin = Admin::findOrFail(4);
+         if ($pharm_approval_2 != 0) {
+             SendSMS::sendMessage('Hi '.$pharm_approval2_admin->full_name.',You have '.$pharm_approval_2.' pending reports to sign. Kindly login to the drug analysis system and complete the process.',$pharm_approval2_admin->tell);
+         }
 
 
-    // }
+        $this->info('SMS Sent');
+
+
+    }
 }
