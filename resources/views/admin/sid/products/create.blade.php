ed2ebdbde635ddd09c68c0ba7055f402e3cbb6f8
Hemen Mike <hemengmike@gmail.com>
2021-05-12 14:57:43 +0000

    change to single lab

--- resources/views/admin/sid/products/create.blade.php
+++ resources/views/admin/sid/products/create.blade.php
@@ -347,7 +347,7 @@
 									        <tr>
                                                 <th></th>
 									            <th>Code</th>
-									            <th style="width:50px">Product Name</th>
+									            <th  style="width:50px">Product Name</th>
 									            <th>Product Type</th>
 									            <th>Customer</th>
                                                 <th>Amt Paid</th>
@@ -389,9 +389,9 @@
                                                 <div class="table-actions">
                                                 {!! $product->show_tag !!}
 
-                                        
+                                                @if ($product->overall_status <1)
                                                 {!! $product->edit_tag !!}
-                                                
+                                                @endif
                                                 <a href="{{route('admin.sid.product.account.index',['id' => $product->id, 'price' => $product->price])}}"> 
                                                 <button type="button" class="btn btn-icon btn-info"><i class="ik ik-dollar-sign"></i></button>    
                                                 </a>
