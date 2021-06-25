162fb3d76daa0ae1a364c669714275a7f2c10c4a
Hemen Mike <hemengmike@gmail.com>
2021-06-25 16:05:34 +0000

    micro update report template 2

--- resources/views/admin/micro/temp/meconclusioninput.blade.php
+++ resources/views/admin/micro/temp/meconclusioninput.blade.php
@@ -7,13 +7,13 @@
     <div class="form-check">
     <input class="form-check-input" type="radio" name="efficacy_default_conclusion" id="flexRadioDefault_1"  {{$product->micro_ea_conclusion == 1 ? "checked":""}}  value="1">
     <label class="form-check-label" for="flexRadioDefault_1">
-       The product showed antimicrobial activity.
+         The product showed antimicrobial activity.
     </label>
   </div>
   <div class="form-check">
     <input class="form-check-input" type="radio" name="efficacy_default_conclusion" id="flexRadioDefault_2"  {{$product->micro_ea_conclusion == 2 ? "checked":""}}  value="2">
     <label class="form-check-label" for="flexRadioDefault_2">
-      The product did not show antimicrobial activity.
+        The product did not show antimicrobial activity.
     </label>
   </div>
       </div>
@@ -28,8 +28,8 @@
         <div class="form-group 432">
         <select  name="micro_ea_comment_option" class="form-control" id="micro_ea_comment_option">
           <option value="{{$product->micro_ea_comment}}">{{$product->micro_ea_comment == Null ?'Please select option':$product->micro_ea_comment}}</option>
-          <option tag2="1" value="The product did not show antimicrobial activity.">The product did not show antimicrobial activity.</option>
-          <option tag2="2" value="The product showed antimicrobial activity.">The product showed antimicrobial activity.</option>
+          <option tag2="1" value="The product showed antimicrobial activity."> The product showed antimicrobial activity.</option>
+          <option tag2="2" value="The product did not show antimicrobial activity.">The product did not show antimicrobial activity.</option>
       </select>
      </div>
       <div class="form-group 432" style="display: none">
