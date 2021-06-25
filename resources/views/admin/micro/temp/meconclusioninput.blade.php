e25d6f156f99139e23ab45e3434ee9aed7ef587f
Hemen Mike <hemengmike@gmail.com>
2021-06-25 15:59:12 +0000

    micro update report template

--- resources/views/admin/micro/temp/meconclusioninput.blade.php
+++ resources/views/admin/micro/temp/meconclusioninput.blade.php
@@ -7,13 +7,13 @@
     <div class="form-check">
     <input class="form-check-input" type="radio" name="efficacy_default_conclusion" id="flexRadioDefault_1"  {{$product->micro_ea_conclusion == 1 ? "checked":""}}  value="1">
     <label class="form-check-label" for="flexRadioDefault_1">
-       The product did not show antimicrobial activity.
+       The product showed antimicrobial activity.
     </label>
   </div>
   <div class="form-check">
     <input class="form-check-input" type="radio" name="efficacy_default_conclusion" id="flexRadioDefault_2"  {{$product->micro_ea_conclusion == 2 ? "checked":""}}  value="2">
     <label class="form-check-label" for="flexRadioDefault_2">
-      The product showed antimicrobial activity.
+      The product did not show antimicrobial activity.
     </label>
   </div>
       </div>
