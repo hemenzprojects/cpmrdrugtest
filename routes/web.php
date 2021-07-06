102831faf8f9d02db2660e8323286d736765ff21
Hemen Mike <hemengmike@gmail.com>
2021-07-06 10:16:37 +0000

    sid chnages

--- routes/web.php
+++ routes/web.php
@@ -124,6 +124,11 @@ Route::get('/sendtest', function () {
    Route::post('sid/hod_office/phyto_completed_report/update','AdminAuth\SID\SIDController@phyto_completedreport_update')->name('admin.sid.phyto_completed_report.update');
    Route::post('sid/hod_office/pharm_completed_report/update','AdminAuth\SID\SIDController@pharm_completedreport_update')->name('admin.sid.pharm_completed_report.update');
 
+ 
+   Route::get('sid/hod_office/micro_completed_reports','AdminAuth\SID\SIDController@micro_completed_reports')->name('admin.sid.micro_completed_reports');
+   Route::get('sid/hod_office/phyto_completed_reports','AdminAuth\SID\SIDController@phyto_completed_reports')->name('admin.sid.phyto_completed_reports');
+   Route::get('sid/hod_office/pharm_completed_reports','AdminAuth\SID\SIDController@pharm_completed_reports')->name('admin.sid.pharm_completed_reports');
+
    
   });
 
@@ -137,12 +142,7 @@ Route::get('/sendtest', function () {
    Route::post('permissions/update', 'AdminAuth\SID\SIDController@permissions_update')->name('admin.permissions.update');
    Route::get('sid/user/{id}/edit','AdminAuth\SID\SIDController@user_editadmin')->name('admin.sid.user.edit');
    Route::post('sid/user/{id}/update','AdminAuth\SID\SIDController@user_updateadmin')->name('admin.sid.user.update');
-
- 
-   Route::get('sid/hod_office/micro_completed_reports','AdminAuth\SID\SIDController@micro_completed_reports')->name('admin.sid.micro_completed_reports');
-   Route::get('sid/hod_office/phyto_completed_reports','AdminAuth\SID\SIDController@phyto_completed_reports')->name('admin.sid.phyto_completed_reports');
-   Route::get('sid/hod_office/pharm_completed_reports','AdminAuth\SID\SIDController@pharm_completed_reports')->name('admin.sid.pharm_completed_reports');
-
+  
   });
 
    //************************************************************* Microbiology Route *******************************************************
