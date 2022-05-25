19a68a59c518fe256966151b35c77f1ae66ad740
hemengmike <hemengmike@gmail.com>
2022-05-25 17:06:36 +0000

    pharm animal house issue changes

--- resources/views/admin/pharm/animalexperiment/editanimaltest.blade.php
+++ resources/views/admin/pharm/animalexperiment/editanimaltest.blade.php
@@ -137,7 +137,7 @@
                                     </tr>
                                 </thead>
                                 <tbody>
-                                    @foreach ($editexperiment->animalExperiment()->orderBy('id', 'desc')->get(); as $product)
+                                    @foreach ($editexperiment->animalExperiment()->orderBy('id','ASC')->get(); as $product)
 
                                     <tr>
                                         <td class="font">                              
