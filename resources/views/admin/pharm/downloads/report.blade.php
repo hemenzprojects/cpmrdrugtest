71a0320f29fad40c48758a939f644d119a8e9a6d
hemengmike <hemengmike@gmail.com>
2022-06-28 11:57:10 +0000

    pharm update

--- resources/views/admin/pharm/downloads/report.blade.php
+++ resources/views/admin/pharm/downloads/report.blade.php
@@ -124,9 +124,7 @@ td, th {
 <tr>
     <th class="font" style="border:0px #fff">
         <span style="font-size:15px">RESULTS: </span><br>
-        @if ($completed_report->pharm_testconducted == 3)
-        <span style="font-size:15.5px"><strong>{{\App\PharmTestConducted::find(2)->name}} </strong></span> <br><br>
-        @endif
+      
     </th>
 
 </tr>
