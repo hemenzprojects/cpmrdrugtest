5fc0ad73f6211cb3f6b2252b23c2c80419f91d8f
hemengmikedev <michael.himeng@competence.team>
2025-02-03 22:54:41 +0000

    fix/ microbiology symbol on report

--- resources/views/admin/micro/downloads/report.blade.php
+++ resources/views/admin/micro/downloads/report.blade.php
@@ -68,9 +68,9 @@ td, th {
 </head>
 
 <body class="watermarked1">
-    <?php 
-    $product = \App\Product::find($report_id); 
-    
+    <?php
+    $product = \App\Product::find($report_id);
+
     ?>
 
 {{-- <img src="{{asset('admin/img/logo.jpg')}}" alt="Paris" class="center">
@@ -136,7 +136,7 @@ td, th {
     <td class="{{($microbial_efficacyanalyses) && count($microbial_efficacyanalyses)>0 ?'font':'font1'}}">{!! $product->micro_analysed_date !!}</td>
 
   </tr>
- 
+
 </table>
 
 @if (($microbial_loadanalyses) && count($microbial_loadanalyses)>0)
@@ -151,7 +151,7 @@ td, th {
   </tr>
   </table>
     <table>
-      
+
         <tr>
             <th class="{{($microbial_efficacyanalyses) && count($microbial_efficacyanalyses)>0 ?'font':'font1'}}">Test </th>
             @if ($product->productType->state ==2)
@@ -178,21 +178,21 @@ td, th {
             // echo '<sup>';  print_r($test_conducted[0]);echo '</sup>';  print_r($test_conducted[1]);  print_r($test_conducted[2]); echo '<sup>'; print_r($test_conducted[3]);  echo '</sup>'; print_r($test_conducted[4]); print_r($test_conducted[5]);
             // }else {
             //     $test_conducted =  $microbial_loadanalyses[$i]->test_conducted;
-            //     print_r($test_conducted); 
-            // }  
-             
+            //     print_r($test_conducted);
+            // }
+
           ?>
            <span> {!! $microbial_loadanalyses[$i]->test_conducted !!}</span>
 
               <input type="hidden" id="rstotal{{$i}}" value="{{$microbial_loadanalyses[$i]->rs_total}}">
 
         </td>
-      
+
 
         <td class="{{($microbial_efficacyanalyses) && count($microbial_efficacyanalyses)>0 ?'font':'font1'}}">
 
           @if ($microbial_loadanalyses[$i]->rs_total <1 || $microbial_loadanalyses[$i]->rs_total == 9900000000)
-            <?php 
+            <?php
             if ($i<2) {
               if ($microbial_loadanalyses[$i]->rs_total == 0) {
                 print_r(' 0 ');
@@ -200,47 +200,49 @@ td, th {
               if ($microbial_loadanalyses[$i]->rs_total == 9900000000) {
                 print_r(' 3 TNTC ');
               }
-              
+
             }
             else {
             $results =  $microbial_loadanalyses[$i]->result;
-            print_r($results); 
+            print_r($results);
             }
             ?>
         @endif
         @if ($microbial_loadanalyses[$i]->rs_total >0 && $microbial_loadanalyses[$i]->rs_total < 9900000000)
-        <span> 
-          <?php 
+        <span>
+          <?php
           if ($i<2) {
               $results= explode(' ',$microbial_loadanalyses[$i]->result);
               $rs_part1 =$results[0];
               $rs_part2 = explode('^',$results[2]);
-          
+
               print_r($rs_part1);  print_r(' x '); print_r($rs_part2[0]);  echo '<sup>';  print_r($rs_part2[1]);
-              
+
           }
           else {
           $results =  $microbial_loadanalyses[$i]->result;
-          print_r($results); 
+          print_r($results);
           }
           ?>
-        </span> 
+        </span>
         @endif
 
 
         </td>
         <td class="{{($microbial_efficacyanalyses) && count($microbial_efficacyanalyses)>0 ?'font':'font1'}}">
-            <?php 
+            <span> </span>
+            <?php
             if ($i<2) {
+               echo ('Not More ');
               $acceptance_criterion= explode(' ',$microbial_loadanalyses[$i]->acceptance_criterion);
               $rs_part1 =$acceptance_criterion[0];
               $rs_part2 = explode('^',$acceptance_criterion[2]);
-        
+
               print_r($rs_part1);  print_r(' x '); print_r($rs_part2[0]);  echo '<sup>';  print_r($rs_part2[1]);
-              
+
             }else {
               $acceptance_criterion =  $microbial_loadanalyses[$i]->acceptance_criterion;
-              print_r($acceptance_criterion); 
+              print_r($acceptance_criterion);
             }
           ?>
             {{-- {{($item->acceptance_criterion)}} --}}
@@ -248,20 +250,18 @@ td, th {
 
         <td class="{{($microbial_efficacyanalyses) && count($microbial_efficacyanalyses)>0 ?'font':'font1'}}">
             {!! $microbial_loadanalyses[$i]->micro_compliance_report !!}
-        </td>                        
+        </td>
       </tr>
       @endfor
     </table>
-
-
 
     <table style="border:0px" >
       <tr style="border:0px ">
       <td style="font-style: italic;font-size:11px;border:0px; width:200px ">
         @for ($i = 0; $i < count($microbial_loadanalyses); $i++)
       @if ($i<1)
-      <span> 
-      {!! $microbial_loadanalyses[0]->definition !!} 
+      <span>
+      {!! $microbial_loadanalyses[0]->definition !!}
       </span>
       @endif
       @endfor
@@ -269,27 +269,27 @@ td, th {
   <td style="font-style: italic;font-size:11px;border:0px; width:200px">
     @for ($i = 0; $i < count($microbial_loadanalyses); $i++)
   @if ($i<1)
-   <span> 
-  {!! $microbial_loadanalyses[1]->definition !!}  
+   <span>
+  {!! $microbial_loadanalyses[1]->definition !!}
   </span>
   @endif
   @endfor
 </td>
   <td style="font-style: italic; font-size:11px;border:0px">
-    
+
 @for ($i = 0; $i < count($microbial_loadanalyses); $i++)
 @if ($i < 1)
   @if ($microbial_loadanalyses[$i]->rs_total == 9900000000)
   <span ><sup>3</sup> TNTC = Too Numerous To Count</span>
-  @endif 
-@endif 
+  @endif
+@endif
 @endfor
   </td>
 </tr>
 
 </table>
 
-<span class="{{($microbial_efficacyanalyses) && count($microbial_efficacyanalyses)>0 ?'font':'font1 '}}"> 
+<span class="{{($microbial_efficacyanalyses) && count($microbial_efficacyanalyses)>0 ?'font':'font1 '}}">
   <strong> General Comment:</strong>
   {{$product->micro_la_comment}}
 </span>
@@ -342,19 +342,19 @@ td, th {
   @endif
   @endfor
 
-  <span style="font-size: 14px;margin-top:2%" ><strong>General Comment:</strong></span> 
+  <span style="font-size: 14px;margin-top:2%" ><strong>General Comment:</strong></span>
   <span style="font-size:14px">{{$product->micro_ea_comment}}</span><br><br>
   @endif
 
   <span class="{{($microbial_efficacyanalyses) && count($microbial_efficacyanalyses)>0 ?'font':'font1'}}">
-    <strong>General Conclusion:</strong> 
+    <strong>General Conclusion:</strong>
     </span>
-    
+
     <span style="font-size:15px">  {{$product->micro_general_conclusion}}</span>
-  
+
 
 <table style="{{($microbial_efficacyanalyses) && count($microbial_efficacyanalyses)>0 ?'margin-top:1%':'margin-top:4%'}}" >
-  
+
   <tr class="{{($microbial_efficacyanalyses) && count($microbial_efficacyanalyses) < 1 ?'space':'space1'}}">
     <td class="font " style="border: 0px solid">
         <?php
@@ -366,12 +366,12 @@ td, th {
         <img src="{{asset(\App\Admin::find($micro_approved_by)? \App\Admin::find($micro_approved_by)->sign_url:'')}}" class="" width="17%" style="margin-bottom: -16px"><br>
         @endif
         -----------------------------<br>
-      
+
         <span>{{ucfirst(\App\Admin::find($micro_approved_by)? \App\Admin::find($micro_approved_by)->full_name:'')}}</span><br>
         <span>{{ucfirst(\App\Admin::find($micro_approved_by)? \App\Admin::find($micro_approved_by)->position:'')}}</span>
     </td>
     <td class="font" style="width: 53%; border: 0px solid"> </td>
-    
+
     <td class="font" style=" border: 0px solid">
         <?php
         $micro_finalapproved_by = ($product? $product->micro_finalapproved_by:'');
@@ -383,15 +383,15 @@ td, th {
         <img src="{{asset(\App\Admin::find($micro_finalapproved_by)? \App\Admin::find($micro_finalapproved_by)->sign_url:'')}}" class="" width="15%" style="margin-bottom: -16px"><br>
         @endif
 
-        ------------------------------<br> 
-        
+        ------------------------------<br>
+
         <span>{{ucfirst(\App\Admin::find($micro_finalapproved_by)? \App\Admin::find($micro_finalapproved_by)->full_name:'')}}</span><br>
         <span>{{ucfirst(\App\Admin::find($micro_finalapproved_by)? \App\Admin::find($micro_finalapproved_by)->position:'')}}</span>
 
     </td>
 
   </tr>
- 
+
 </table>
 </div>
 </div>
